abeda05bd8254cb4e8178e00502343b074847cdb
Nic Dahlquist <nic@snapchat.com>
2014-12-26 18:07:09 -0800

    Closes and deletes PRs during merges, and links between comments and PRs.
    Closes #11
    
    ---
    Squash commit from https://github.com/ndahlquist/Whaler/pull/12 created by Whaler.

--- appengine/QueueMergeEndpoint.py
+++ appengine/QueueMergeEndpoint.py
@@ -23,11 +23,11 @@ class QueueMergeEndpoint(webapp2.RequestHandler):
         self.response.headers.add_header('Access-Control-Allow-Origin', '*')
 
         # Parse the repo name, owner and PR issue number from the referer URL.
-        referer_url = self.request.headers['Referer']
-        split_referer = referer_url.split('/')
-        owner_name = split_referer[3]
-        repo_name = split_referer[4]
-        issue_number = int(split_referer[6])
+        pull_request_url = self.request.headers['Referer']
+        split_url = pull_request_url.split('/')
+        owner_name = split_url[3]
+        repo_name = split_url[4]
+        issue_number = int(split_url[6])
         logging.info("owner=%s, repo=%s, issue=%s" % (owner_name, repo_name, issue_number))
 
         repo = GitHubRepo(owner_name, repo_name)
@@ -46,14 +46,24 @@ class QueueMergeEndpoint(webapp2.RequestHandler):
             head_commit = repo.repo.get_commit(pull.head.sha)
             tree = head_commit.commit.tree
 
-        squash_commit_message = "%s\n\nSquash commit created by Whaler." % self.request.get('commit_message')
+        squash_commit_message = "%s\n" % self.request.get('commit_message') + \
+                                "\n" \
+                                "---\n" \
+                                "Squash commit from %s created by Whaler." % pull_request_url
         parents = [repo.repo.get_git_commit(pull.base.sha)]
         author = self.get_git_author(pull)
         committer = author  # TODO: Might be better if this is the person who presses the button.
         new_commit = repo.repo.create_git_commit(squash_commit_message, tree, parents, author, committer)
 
-        branch_base = repo.repo.get_git_ref("heads/%s" % base)
-        branch_base.edit(new_commit.sha)
+        base_branch = repo.repo.get_git_ref("heads/%s" % base)
+        base_branch.edit(new_commit.sha)
+
+        pull.create_issue_comment("Merged via squash commit %s. :whale:" % new_commit.sha)
+        pull.edit(state='closed')
+
+        # Delete the merged branch.
+        head_branch = repo.repo.get_git_ref("heads/%s" % head)
+        head_branch.delete()
 
     @staticmethod
     def get_git_author(pull_request):
