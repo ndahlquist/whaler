81459e1bdda2e8700bcfe0c6c6c52140106bb20c
Nic Dahlquist <nic@snapchat.com>
2014-12-26 23:54:26 -0800

    Change the github merge button to say "Squash merge".
    ---
    Squash commit from https://github.com/ndahlquist/Whaler/pull/26 created by Whaler.

--- chrome-extension/content_script.js
+++ chrome-extension/content_script.js
@@ -1,43 +1,47 @@
 /**
- * Our CSS colors the merge buttons blue.
+ * Our CSS makes the merge buttons blue.
  */
 function injectCSS() {
   var link = document.createElement('link');
   link.href = chrome.extension.getURL('css/whaler.css');
   link.rel = 'stylesheet';
-  head = document.getElementsByTagName ("head")[0] || document.documentElement;
+  head = document.getElementsByTagName ('head')[0] || document.documentElement;
   head.insertBefore(link, head.lastChild);
 }
 
-function modifyMergeButton() {
-  button = document.querySelector(".merge-branch-action");
-  if (button == null) return;
-  // Add our CSS tag to paint the button blue.
-  button.className = button.className.replace(/primary/g, 'whaler');
-  button.onclick = modifyMergeForm
+function modifyDocument() {
+  mergeButton = document.querySelector('.merge-branch-action');
+  if (mergeButton != null) {
+    // Add our CSS tag to paint the button blue.
+    mergeButton.className = mergeButton.className.replace(/primary/g, 'whaler');
+    // Modify the text
+    mergeButton.innerHTML = mergeButton.innerHTML.replace(/Merge pull request/g, 'Squash merge')
+  }
+
+  form = document.querySelector('.merge-branch-form');
+  if (form != null) {
+    // Redirect the form to our server so that we can handle the merge.
+    form.action = 'https://whaler-on-fleek.appspot.com/queue_merge';
+
+    confirmMergeButton = form.querySelector('.button');
+    // Add our CSS tag to paint the button blue.
+    confirmMergeButton.className = confirmMergeButton.className.replace(/primary/g, 'whaler');
+  }
 }
 
-function modifyMergeForm() {
-  form = document.querySelector(".merge-branch-form");
-  if (form == null) return;
-  // Redirect the form to our server, so that we can handle the merge.
-  form.action = "https://whaler-on-fleek.appspot.com/queue_merge";
+function domNodeInsertedCallback() {
+    console.log('DomNodeInsertedCallback()');
 
-  confirmMergeButton = form.querySelector(".button");
-  // Add our CSS tag to paint the button blue.
-  confirmMergeButton.className = confirmMergeButton.className.replace(/primary/g, 'whaler');
+    // We must remove the event listener before modifyDocument() to prevent recursion.
+    document.removeEventListener('DOMNodeInserted', domNodeInsertedCallback);
+    modifyDocument();
+    document.addEventListener('DOMNodeInserted', domNodeInsertedCallback);
 }
 
 injectCSS();
-
-modifyMergeButton();
-modifyMergeForm();
+modifyDocument();
 
 // GitHub dynamically modifies the merge form. We need to make sure we apply our modifications when it is updated.
 // TODO: This is allegedly terrible for performance. What's a better way of doing this?
-document.addEventListener('DOMNodeInserted', function() {
-    console.log('DOMNodeInserted event.');
-    modifyMergeButton();
-    modifyMergeForm();
-}, true);
+document.addEventListener('DOMNodeInserted', domNodeInsertedCallback);
 
