b9574b552e5335e90ab8e59637edc9e05736fd2c
Nic Dahlquist <nic@snapchat.com>
2014-12-27 16:56:34 -0800

    Added memcache for /supports_whaler. Closes #36.
    ---
    Squash commit from https://github.com/ndahlquist/whaler/pull/37 created by Whaler.

--- appengine/SupportsWhalerEndpoint.py
+++ appengine/SupportsWhalerEndpoint.py
@@ -2,6 +2,8 @@
 import webapp2
 import logging
 
+from google.appengine.api import memcache
+
 from GitHubRepo import GitHubRepo
 
 
@@ -20,10 +22,27 @@ class SupportsWhalerEndpoint(webapp2.RequestHandler):
         repo_name = split_url[4]
         logging.info("owner=%s, repo=%s" % (owner_name, repo_name))
 
-        try:
-            GitHubRepo(owner_name, repo_name)
+        if has_access(owner_name, repo_name):
             self.response.body = "supported"
-        except:
+        else:
             url = "https://github.com/%s/%s/settings/collaboration" % (owner_name, repo_name)
             self.response.body = "This repo does not support squash commits! " \
                                  "<a href=\"%s\">Add Whaler</a> as a collaborator." % url
+
+
+def has_access(owner_name, repo_name):
+    memcache_key = "supports %s/%s" % (owner_name, repo_name)
+    memcache_entry = memcache.get(key=memcache_key)
+    if memcache_entry == 'true':
+        return True
+    if memcache_entry == 'false':
+        return False
+
+    try:
+        GitHubRepo(owner_name, repo_name)
+        memcache.add(key=memcache_key, value='true', time=60*60*24)
+        return True
+    except:
+        # TODO: It will take up to 2 minutes for Whaler to 'see' new repos. This is hacky.
+        memcache.add(key=memcache_key, value='false', time=2*60)
+        return False
