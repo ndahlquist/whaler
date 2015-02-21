efa8b8a40420e003cf4e931ce512ab574e28b32b
Nic Dahlquist <nic@snapchat.com>
2015-02-21 13:40:37 -0800

    Removed redundant data model fields.
    ---
    Squash commit from https://github.com/ndahlquist/whaler/pull/84 created by Whaler.

--- appengine/datamodel.py
+++ appengine/datamodel.py
@@ -8,7 +8,6 @@ class SessionAuthEntry(ndb.Model):
     Each user should have one or more of these entries, parented by a UserEntry.
     """
     timestamp = ndb.DateTimeProperty(auto_now_add=True)
-    session_token = ndb.StringProperty()
 
 
 class UserEntry(ndb.Model):
@@ -17,7 +16,6 @@ class UserEntry(ndb.Model):
     Each user should have exactly one of these entries.
     """
     timestamp = ndb.DateTimeProperty(auto_now_add=True)
-    username = ndb.StringProperty()
     oauth_token = ndb.StringProperty()
 
     @classmethod
@@ -30,9 +28,9 @@ class UserEntry(ndb.Model):
         if oauth_entry is None:
             return None
 
-        session_auth_entries = SessionAuthEntry.query(ancestor=oauth_entry.key).fetch()
-        for session_auth_entry in session_auth_entries:
-            if session_token == session_auth_entry.session_token:
+        session_auth_keys = SessionAuthEntry.query(ancestor=oauth_entry.key).fetch(keys_only=True)
+        for session_auth_key in session_auth_keys:
+            if session_token == session_auth_key.id():
                 return oauth_entry
 
         logging.warning('No matching session token found: %s' % session_token)
@@ -46,8 +44,8 @@ class UserEntry(ndb.Model):
         """
         oauth_entry = UserEntry.get_by_id(username)
         if oauth_entry is None:
-            oauth_entry = UserEntry(id=username, username=username, oauth_token=oauth_token)
+            oauth_entry = UserEntry(id=username, oauth_token=oauth_token)
             oauth_entry.put()
 
-        session_auth_entry = SessionAuthEntry(id=session_token, session_token=session_token, parent=oauth_entry.key)
+        session_auth_entry = SessionAuthEntry(id=session_token, parent=oauth_entry.key)
         session_auth_entry.put()
