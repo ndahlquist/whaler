1629e598733f264c5d3b707396ab007d318cb0b8
Nic Dahlquist <nic@snapchat.com>
2015-04-09 17:21:49 -0700

    Log bad requests to /merge.

--- appengine/MergeEndpoint.py
+++ appengine/MergeEndpoint.py
@@ -20,55 +20,59 @@ class MergeEndpoint(webapp2.RequestHandler):
         self.response.headers.add_header('Access-Control-Allow-Origin', '*')
 
     def post(self):
-        self.response.headers.add_header('Access-Control-Allow-Origin', '*')
+        try:
+            self.response.headers.add_header('Access-Control-Allow-Origin', '*')
 
-        username = self.request.get('username')
-        session_token = self.request.get('session_token')
-        user_entry = UserEntry.lookup(username, session_token)
-        assert user_entry is not None
+            username = self.request.get('username')
+            session_token = self.request.get('session_token')
+            user_entry = UserEntry.lookup(username, session_token)
+            assert user_entry is not None
 
-        # Parse the repo name, owner and PR issue number from the referer URL.
-        pull_request_url = self.request.headers['Referer']
-        split_url = pull_request_url.split('/')
-        owner_name = split_url[3]
-        repo_name = split_url[4]
-        issue_number = int(split_url[6])
-        logging.info("owner=%s, repo=%s, issue=%s" % (owner_name, repo_name, issue_number))
+            # Parse the repo name, owner and PR issue number from the referer URL.
+            pull_request_url = self.request.headers['Referer']
+            split_url = pull_request_url.split('/')
+            owner_name = split_url[3]
+            repo_name = split_url[4]
+            issue_number = int(split_url[6])
+            logging.info("owner=%s, repo=%s, issue=%s" % (owner_name, repo_name, issue_number))
 
-        repo = GitHubRepo(user_entry.oauth_token, owner_name, repo_name)
+            repo = GitHubRepo(user_entry.oauth_token, owner_name, repo_name)
 
-        pull = repo.repo.get_pull(issue_number)
-        head = pull.head.ref
-        base = pull.base.ref
+            pull = repo.repo.get_pull(issue_number)
+            head = pull.head.ref
+            base = pull.base.ref
 
-        # Merge base into head
-        merge_commit = repo.repo.merge(head, base)
+            # Merge base into head
+            merge_commit = repo.repo.merge(head, base)
 
-        if merge_commit:
-            # TODO: Wait for CI build to pass (use git hook)
-            tree = merge_commit.commit.tree
-        else:
-            head_commit = repo.repo.get_commit(pull.head.sha)
-            tree = head_commit.commit.tree
+            if merge_commit:
+                # TODO: Wait for CI build to pass (use git hook)
+                tree = merge_commit.commit.tree
+            else:
+                head_commit = repo.repo.get_commit(pull.head.sha)
+                tree = head_commit.commit.tree
 
-        squash_commit_message = "%s\n" % self.request.get('commit_message') + \
-                                "\n" \
-                                "---\n" \
-                                "Squash commit from %s created by Whaler." % pull_request_url
-        base_branch = repo.repo.get_git_ref("heads/%s" % base)
-        parents = [repo.repo.get_git_commit(base_branch.object.sha)]
-        author = self.get_author(pull)
-        committer = self.get_committer(repo)
-        new_commit = repo.repo.create_git_commit(squash_commit_message, tree, parents, author, committer)
+            squash_commit_message = "%s\n" % self.request.get('commit_message') + \
+                                    "\n" \
+                                    "---\n" \
+                                    "Squash commit from %s created by Whaler." % pull_request_url
+            base_branch = repo.repo.get_git_ref("heads/%s" % base)
+            parents = [repo.repo.get_git_commit(base_branch.object.sha)]
+            author = self.get_author(pull)
+            committer = self.get_committer(repo)
+            new_commit = repo.repo.create_git_commit(squash_commit_message, tree, parents, author, committer)
 
-        base_branch.edit(new_commit.sha)
+            base_branch.edit(new_commit.sha)
 
-        pull.create_issue_comment(self.MERGE_MESSAGE % new_commit.sha)
-        pull.edit(state='closed')
+            pull.create_issue_comment(self.MERGE_MESSAGE % new_commit.sha)
+            pull.edit(state='closed')
 
-        # Delete the merged branch.
-        head_branch = repo.repo.get_git_ref("heads/%s" % head)
-        head_branch.delete()
+            # Delete the merged branch.
+            head_branch = repo.repo.get_git_ref("heads/%s" % head)
+            head_branch.delete()
+        except:
+            logging.debug(repr(self.request.headers))
+            raise
 
     def get_committer(self, repo):
         """
